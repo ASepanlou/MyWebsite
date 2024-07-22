3157a846aa030d813e7712f0756ce0fea0a967a5
Abtin Sepanlou <abtin.sepanlou@gmail.com>
2024-07-22 02:15:26 -0400

    Removed redundant logo.svg and added sidebar functionality basics

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -1,17 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isSidebarVisible, setSidebarVisibility] = useState(false);
+  const [isTopButtonVisible, setTopButtonVisibility] = useState(false);
   return (
     <div>
-      <div className='navbar'>
+      <div id='navbar'>
         <span style={{fontSize: "50px"}} className='material-symbols-outlined'>eyeglasses</span>
+        <a href='#about'>About me</a>
         <a href='#skills'>Skills</a>
-        <a href='#about'>About</a>
-        <button id="menu-button" className='material-symbols-outlined'>menu</button>
+        <a href='#experience'>Experience</a>
+        <a href='#education'>Education</a>
+        <a href='#contact'>Contact</a>
+        <button id="menu-button" className='material-symbols-outlined' onClick={() => setSidebarVisibility(true)}>menu</button>
+      </div>
+      <div id='sidebar' style={{display: isSidebarVisible ? "flex" : "none"}}>
+        <button style={{alignSelf: "start", color: "black"}} id="close-button" className='material-symbols-outlined' onClick={() => setSidebarVisibility(false)}>close</button>
+        <a href='#about'>About me</a>
+        <a href='#skills'>Skills</a>
+        <a href='#experience'>Experience</a>
+        <a href='#education'>Education</a>
+        <a href='#contact'>Contact</a>
       </div>
       <div id='top'></div>
-      <a id="top-button" href='#top' className='material-symbols-outlined'>arrow_upward</a>
+      <a id="top-button" href='#top' className='material-symbols-outlined' style={{display: isTopButtonVisible ? "inline" : "none"}}>arrow_upward</a>
     </div>
   );
 }
